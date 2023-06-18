0a7520cef3f0b1eb0e99915927865c7dbf1a9e7c
SIIIKOR <siiikor.sikorski@gmail.com>
2023-06-18 14:30:33 +0200

    made dataset class and imported model

---
+++ main.py
@@ -0,0 +1,30 @@
+from transformers import pipeline
+from torch.utils.data import Dataset
+
+
+class EnPrestoDataset(Dataset):
+    def __init__(self):
+        self.path = "data/en_data.txt"
+        self.data = []
+        with open(self.path, "r") as f:
+            for line in f:
+                for sentence in line.split(" # "):
+                    if len(sentence) > 0:
+                        if sentence[-1] == "\n":
+                            sentence = sentence[:-1]
+                        self.data.append(sentence)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+
+if __name__ == "__main__":
+    dataset = EnPrestoDataset()
+    max_length = len(max(dataset, key=lambda x: len(x)))
+    model_checkpoint = "sdadas/mt5-base-translator-en-pl"
+    translator = pipeline("translation", model=model_checkpoint, device=0)
+    for out in translator(dataset, max_length=max_length):
+        print(out)
