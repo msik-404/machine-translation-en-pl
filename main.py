50d7574e6b5f7ce29fbb6509878bb60cd5af02e6
SIIIKOR <siiikor.sikorski@gmail.com>
2023-06-25 14:18:58 +0200

    output now is in the same format as input, that is it has #

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 from torch.utils.data import Dataset, DataLoader
 
+from collections import deque
+
 EN_PATH = "data/en_data.txt"
 PL_PATH = "data/pl_data.txt"
 
@@ -12,13 +14,17 @@ class EnPrestoDataset(Dataset):
     def __init__(self, tokenizer):
         self.path = EN_PATH
         text = []
+        self.hash_counts = deque()
         with open(self.path, "r", encoding="utf-8") as f:
-            for line in f:
-                for sentence in line.split(" # "):
-                    if len(sentence) > 0:
-                        if sentence[-1] == "\n":
-                            sentence = sentence[:-1]
-                        text.append(sentence)
+            for dialog in f:
+                sentences = dialog.split(" # ")
+                self.hash_counts.append(len(sentences))
+                for sentence in sentences:
+                    if len(sentence) == 0:
+                        sentence = ""
+                    elif sentence[-1] == "\n":
+                        sentence = sentence[:-1]
+                    text.append(sentence)
         self.inputs = tokenizer(
             text,
             max_length=MAX_LENGTH,  # avg sentence length is 36.8
@@ -44,6 +50,8 @@ if __name__ == "__main__":
     )
     dataloader = DataLoader(dataset, batch_size=BATCH_SIZE)
 
+    curr_dialog_parts_left = dataset.hash_counts.popleft()
+    curr_dialog = []
     counter = 0
     with open(PL_PATH, "w", encoding="utf-8") as f:
         for data in dataloader:
@@ -54,8 +62,17 @@ if __name__ == "__main__":
                 clean_up_tokenization_spaces=True,
             )
             for output in outputs:
-                f.write(output)
-                f.write("\n")
+                if curr_dialog_parts_left != 1:
+                    curr_dialog.append(output)
+                    curr_dialog.append(" # ")
+                else:
+                    curr_dialog.append(output)
+                curr_dialog_parts_left -= 1
+                if curr_dialog_parts_left == 0:
+                    curr_dialog.append("\n")
+                    f.write("".join(curr_dialog))
+                    curr_dialog = []
+                    curr_dialog_parts_left = dataset.hash_counts.popleft()
 
             counter += 1
             if counter % 10 == 0:
